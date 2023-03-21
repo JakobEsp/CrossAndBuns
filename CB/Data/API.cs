cd9ae736125ef0fda0cba4bcb89f825bcacf227f
WidovV <111432890+Sebastian-tec@users.noreply.github.com>
2023-03-21 23:59:57 +0100

    It actually works now :pray: Read desc. for further
    So i more or less refactored everything multiple times, but a short summary:
    Api.cs:
    Changed domain to just be a string, changed text to a list of dynamic
    
    Fetch.cs:
    Redid my methods since i changed the field types,
    Added checks
    
    FetchData.razor:
    Made some checks for null values,
    Looped in loops, to get the items in the lists
    
    Overall:
    Made it work
    Added comments

--- CB/Data/API.cs
+++ CB/Data/API.cs
@@ -1,41 +1,37 @@
-﻿using Newtonsoft.Json;
-using RestSharp;
-using System.Net;
-using System.Runtime.CompilerServices;
+﻿using System.Net;
 
-namespace CB.Data
+public class API
 {
-    public class API
+    // Create an int id field
+    public int Id { get; set; }
+    // Create a IPAdress list field
+    public List<IPAddress>? IP { get; set; } = new();
+    // Create a string domain field
+    public string? Domain { get; set; } 
+    // Create a dynamic list 
+    public List<dynamic> Text { get; set; } = new();
+    // Create a string days field
+    private string? days { get; set; }
+    // Create a string days property
+    public string? Days
     {
-        public int Id { get; set; }
-        public string[]? IP { get; set; }
-        public string[]? Domain { get; set; }
-
-        public List<string> Text { get; set; } = new();
-
-        private string? days { get; set; }
-
-        public string? Days
+        // Get the value of days
+        get { return days; }
+        // Set the value of days
+        set
         {
-            get { return days; }
-
-            set {
-                if (value == null)
-                    days = "0";
-                else
-                    days = value;
-            }
+            // Check if value is null
+            if (value == null)
+                days = "0"; // Set days to 0
+            else // If value isn't null
+                days = value; // Set days to value
         }
+    }
 
-        private readonly int _id = 0;
+    private readonly int _id = 0; // Create an int id field
 
-        public API()
-        {
-            IP = null;
-            Domain = null;
-            Days = null;
-            Id = Interlocked.Increment(ref _id);
-        }
-
+    public API()
+    {
+        Id = Interlocked.Increment(ref _id); // Increment the id field in the default ctor
     }
 }
--- CB/Data/Fetch.cs
+++ CB/Data/Fetch.cs
@@ -2,55 +2,36 @@
 using RestSharp;
 using static System.Net.Mime.MediaTypeNames;
 using System.Net;
+using System;
 
 namespace CB.Data
 {
     public class Fetch
     {
-        public static void GetIpUrl(API api)
-        {
-            for (int i = 0; i < api.IP?.Length; i++)
-            {
-                IPAddress addr = IPAddress.Parse(api.IP[i]);
-                // Print the ipaddress to the console, debug purposes
-                Console.WriteLine(addr);
-                // Get the host entry from the ipaddress
-                IPHostEntry entry = Dns.GetHostEntry(addr);
-                // If the entry isn't null, might reverse for non nesting
-                if (entry != null)
-                {
-                    // Print the hostname to the console, debug purposes
-                    Console.WriteLine(entry.HostName);
-                    // Print all the ip addresses to the console, debug purposes
-                    foreach (IPAddress ip in entry.AddressList)
-                    {
-                        Console.WriteLine(ip.ToString());
-                    }
-                }
-                // Set the domain field, might change to the domain name instead of "entry.hostname"
-                api.Domain[i] = entry.HostName;
-            }
-
-        }
-
         public static void GetUrlIp(API api)
         {
-            // Create a new Uri object from the url
-            for (int i = 0; i < api.IP?.Length; i++)
+            // Initialise a new Uri object from the domain name
+            Uri myUri = new(api.Domain);
+            // Get the IP address from the domain name
+            IPHostEntry host = Dns.GetHostEntry(myUri.Host);
+            // Foreach IP address in the host entry
+            foreach (IPAddress ip in host.AddressList)
             {
-                Uri? myUri = new(api.Domain[i]);
-                // Get the IP addressArray from the Uri object
-                IPAddress[] ip = Dns.GetHostAddresses(myUri.Host); // I know it's kinda dumb this way, but yeah
-                // Print the IP addressArray to the console, debug purposes
-                api.IP[i] = Convert.ToString(ip[i]);
+                // Check if api.ip list does not contains the current ip
+                if (!api.IP.Contains(ip))
+                {
+                    // Add the ip
+                    api.IP.Add(ip);
+                }
             }
-
         }
 
         public static API? ApiReq(API api)
         {
-            GetUrlIp(api); // Get the IP address from the domain name
-            for (int i = 0; i < api.IP?.Length; i++)
+            // Run the get url ip method with the instance as parameter
+            GetUrlIp(api); 
+            // Foreach loop every ip in the list
+            foreach (IPAddress ip in api.IP)
             {
                 // Initialise a new RestSharp client to the API endpoint
                 RestClient client = new("https://api.abuseipdb.com/api/v2/check");
@@ -59,22 +40,30 @@ namespace CB.Data
 
                 // Add the API key to the request header
                 request.AddHeader("Key", "87de98a405dd35a259ebf321ed41d7c7906417cac5d3a693dc0e2371d03e4a06d2ef29c814ff4b09");
+
                 // Add the Accept header to the request header to specify the response format
                 request.AddHeader("Accept", "application/json");
-                // Add the IP address to the request parameters
-                request.AddParameter("ipAddress", api.IP[i]);
-                // Add the maxAgeInDays parameter to the request parameters, might turn it into a variable later
 
+                // Add the IP address to the request parameters
+                request.AddParameter("ipAddress", Convert.ToString(ip));
+
+                // Add the maxAgeInDays parameter to the request parameters, might turn it into a variable later
                 request.AddParameter("maxAgeInDays", api.Days = api.Days == "0" ? "90" : api.Days);
+
                 // Add the verbose parameter to the request parameters so we get the full response
                 request.AddParameter("verbose", "");
+
                 // Execute the request and store the response
                 RestResponse response = client.Execute(request);
-                // Add the response to the content list
-                api.Text.Add(JsonConvert.DeserializeObject<string>(response.Content));
+
+                // Check if response isn't null
+                if (response != null)
+                {
+                    // Add the response content to the dynamic list
+                    api.Text.Add(JsonConvert.DeserializeObject<dynamic>(response.Content));
+                }
             }
-
-
+            // Return our object
             return api;
         }
     }
